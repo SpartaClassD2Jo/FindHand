acbffd595399cc7ea0bcb351850ac373e0c69f68
Adam Sung Min Park <parksm2@uci.edu>
2022-09-20 09:41:39 +0900

    register page

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from pymongo import MongoClient
 from bs4 import BeautifulSoup
+from db import clie
 from flask import Flask, render_template, request, jsonify,session, make_response
 from jinja2 import Template
 import jwt
 
-app = Flask(__name__)
 
+# 나중에 git-encrypt 사용하여 이부분은 따로 보안 할 예정
+client = MongoClient('mongodb+srv://데이타 베이스 넣기')
+
+app = Flask(__name__)
+db = client.dbsparta
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
+@app.route('/login/submit', methods = ['POST'])
+def logging_in():
+    email_receive = request.form['email_give']
+    password_receive = request.form['password_give']
+    find_user = db."the data base".find({'email':email_receive, 'password':password_receive}, {'_id':False})
+    return jsonify({'msg':find_user})
+
 
 
 @app.route('/')
